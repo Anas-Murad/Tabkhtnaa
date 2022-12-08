4aeff16b63ac80f98c409873cef3f22eab6f940c
Anas-Murad <anas.murad2524@gmail.com>
2022-12-09 00:48:47 +0300

    edit category

--- database/migrations/2022_12_03_165247_create_categories_table.php
+++ database/migrations/2022_12_03_165247_create_categories_table.php
@@ -15,9 +15,8 @@ return new class extends Migration
     {
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('key');
             $table->string('icon');
-            $table->timestamps();
         });
     }
 
--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -3,8 +3,6 @@
 namespace Database\Seeders;
 
 use App\Models\Category;
-use Carbon\Carbon;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class CategorySeeder extends Seeder
@@ -18,26 +16,24 @@ class CategorySeeder extends Seeder
     {
         $data =
             [
-                ['name' => ['en' => 'category1', 'ar' => 'فواكة'], 'icon' => 'https://pos.tecknick.net/icons8-facebook-240.png','created_at' => Carbon::now() , 'updated_at' => Carbon::now()],
-                ['name' => ['en' => 'category2', 'ar' => 'حلويات'], 'icon' => 'https://pos.tecknick.net/icons8-facebook-240.png','created_at' => Carbon::now() , 'updated_at' => Carbon::now()],
-                ['name' => ['en' => 'category3', 'ar' => 'وجبات أرز'], 'icon' => 'https://pos.tecknick.net/icons8-facebook-240.png','created_at' => Carbon::now() , 'updated_at' => Carbon::now()],
-                ['name' => ['en' => 'category4', 'ar' => 'معكرونة'], 'icon' => 'https://pos.tecknick.net/icons8-facebook-240.png','created_at' => Carbon::now() , 'updated_at' => Carbon::now()],
-                ['name' => ['en' => 'category5', 'ar' => 'رغيف خبز'], 'icon' => 'https://pos.tecknick.net/icons8-facebook-240.png','created_at' => Carbon::now() , 'updated_at' => Carbon::now()],
-                ['name' => ['en' => 'category6', 'ar' => 'مقبلات شهية'], 'icon' => 'https://pos.tecknick.net/icons8-facebook-240.png','created_at' => Carbon::now() , 'updated_at' => Carbon::now()],
-                ['name' => ['en' => 'category7', 'ar' => 'مقبلات شهية'], 'icon' => 'https://pos.tecknick.net/icons8-facebook-240.png','created_at' => Carbon::now() , 'updated_at' => Carbon::now()],
-                ['name' => ['en' => 'category8', 'ar' => 'وجبات دجاج شهية'], 'icon' => 'https://pos.tecknick.net/icons8-facebook-240.png','created_at' => Carbon::now() , 'updated_at' => Carbon::now()],
-                ['name' => ['en' => 'category9', 'ar' => 'ودبات دجاج'], 'icon' => 'https://pos.tecknick.net/icons8-facebook-240.png','created_at' => Carbon::now() , 'updated_at' => Carbon::now()],
-                ['name' => ['en' => 'category10', 'ar' => 'فواكه شهية'], 'icon' => 'https://pos.tecknick.net/icons8-facebook-240.png','created_at' => Carbon::now() , 'updated_at' => Carbon::now()],
-                ['name' => ['en' => 'category11', 'ar' => 'حساء الحمة'], 'icon' => 'https://pos.tecknick.net/icons8-facebook-240.png','created_at' => Carbon::now() , 'updated_at' => Carbon::now()],
-                ['name' => ['en' => 'category12', 'ar' => 'ستيك لحمة'], 'icon' => 'https://pos.tecknick.net/icons8-facebook-240.png','created_at' => Carbon::now() , 'updated_at' => Carbon::now()],
-                ['name' => ['en' => 'category13', 'ar' => 'شوربات حارة'], 'icon' => 'https://pos.tecknick.net/icons8-facebook-240.png','created_at' => Carbon::now() , 'updated_at' => Carbon::now()],
-                ['name' => ['en' => 'category14', 'ar' => 'مشاوي'], 'icon' => 'https://pos.tecknick.net/icons8-facebook-240.png','created_at' => Carbon::now() , 'updated_at' => Carbon::now()],
-                ['name' => ['en' => 'category15', 'ar' => 'مشاوي عل فحم'], 'icon' => 'https://pos.tecknick.net/icons8-facebook-240.png','created_at' => Carbon::now() , 'updated_at' => Carbon::now()],
-                ['name' => ['en' => 'category16', 'ar' => 'معكرونة'], 'icon' => 'https://pos.tecknick.net/icons8-facebook-240.png','created_at' => Carbon::now() , 'updated_at' => Carbon::now()],
-                ['name' => ['en' => 'category17', 'ar' => 'رغيف خبرز'], 'icon' => 'https://pos.tecknick.net/icons8-facebook-240.png','created_at' => Carbon::now() , 'updated_at' => Carbon::now()],
-                ['name' => ['en' => 'category18', 'ar' => 'معكرونة'], 'icon' => 'https://pos.tecknick.net/icons8-facebook-240.png','created_at' => Carbon::now() , 'updated_at' => Carbon::now()],
-                ['name' => ['en' => 'category19', 'ar' => 'زغيف خبز'], 'icon' => 'https://pos.tecknick.net/icons8-facebook-240.png','created_at' => Carbon::now() , 'updated_at' => Carbon::now()],
-                ['name' => ['en' => 'category20', 'ar' => 'مقبلات شهية'], 'icon' => 'https://pos.tecknick.net/icons8-facebook-240.png','created_at' => Carbon::now() , 'updated_at' => Carbon::now()],
+                ['key' => 'appetizers', 'icon' => asset('images/categorise/Appetizers-01.png') ],
+                ['key' => 'asian_food', 'icon' => asset('images/categorise/Asian food-01.png') ],
+                ['key' => 'bakery', 'icon' => asset('images/categorise/Bakery-01.png') ],
+                ['key' => 'barbeque', 'icon' => asset('images/categorise/Barbeque-01.png') ],
+                ['key' => 'dessert', 'icon' => asset('images/categorise/Dessert-01.png') ],
+                ['key' => 'drinks', 'icon' => asset('images/categorise/Drinks-01.png') ],
+                ['key' => 'fast_food', 'icon' => asset('images/categorise/Fast food-01.png') ],
+                ['key' => 'frozen', 'icon' => asset('images/categorise/Frozen-01.png') ],
+                ['key' => 'healthy_food', 'icon' => asset('images/categorise/Healthy food-01.png') ],
+                ['key' => 'orders', 'icon' => asset('images/categorise/Orders-01.png') ],
+                ['key' => 'oriental_food', 'icon' => asset('images/categorise/Oriental food-01.png') ],
+                ['key' => 'pasta', 'icon' => asset('images/categorise/Pasta-01.png') ],
+                ['key' => 'pickels', 'icon' => asset('images/categorise/Pickels-01.png') ],
+                ['key' => 'salad', 'icon' => asset('images/categorise/Salad-01.png') ],
+                ['key' => 'sandwiches', 'icon' => asset('images/categorise/Sandwiches-01.png') ],
+                ['key' => 'soup', 'icon' => asset('images/categorise/Soup-01.png') ],
+                ['key' => 'spicy', 'icon' => asset('images/categorise/Spicy-01.png') ],
+                ['key' => 'western', 'icon' => asset('images/categorise/Western-01.png') ],
             ];
         foreach ($data as $d)
         {
